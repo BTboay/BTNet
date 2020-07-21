7a06b9c9a33a217fd5e5de625626c1f9dd005221
btboay <yuzhuoandlb@foxmail.com>
2020-07-21 23:37:02 +0800

    源代码

---
+++ src/image.h
@@ -0,0 +1,15 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+typedef struct image
+{
+    int h;
+    int w;
+    int c;
+    float *data;
+} image;
+
+
+image *load_image_data(char *img_path);
+void save_image_data(image *img, char *savepath);
+#endif
